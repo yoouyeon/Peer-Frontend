01360899eeb16be1044c35305a9cfdd0900f1b15
yoouyeon <jyeon.yoon59@gmail.com>
2025-05-31 14:37:24 +0900

    test: MSW 서버 설정 추가
    - 테스트 실행 전 MSW 서버 시작과 종료 설정
    - 각 테스트 후 핸들러 초기화

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,3 +1,20 @@
 import React from 'react'
 import '@testing-library/jest-dom'
+import { server } from '@/mocks/server'
+
 global.React = React
+
+beforeAll(() => {
+  // 테스트 시작 전 MSW 서버 시작
+  server.listen()
+})
+
+afterEach(() => {
+  // 각 테스트 후 핸들러 초기화
+  server.resetHandlers()
+})
+
+afterAll(() => {
+  // 모든 테스트 완료 후 서버 종료
+  server.close()
+})
